505216c60a73cf994aae5a13dacf9ff3436eae96
GaneshPandey <footballcrazey@gmail.com>
2021-01-31 16:35:39 +0545

    update configuration for api

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,17 @@
+from datetime import timedelta
+
 DEBUG = True
+LOG_LEVEL = 'DEBUG'  # CRITICAL / ERROR / WARNING / INFO / DEBUG
+
+SERVER_NAME = 'localhost:8000'
+SECRET_KEY = 'dev-secret-key'
+
+# SQLAlchemy.
+db_uri = 'postgresql://devuser:devpassword@postgres:5432/recommendation'
+SQLALCHEMY_DATABASE_URI = db_uri
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# User.
+SEED_ADMIN_EMAIL = 'dev@local.host'
+SEED_ADMIN_PASSWORD = 'devpassword'
+REMEMBER_COOKIE_DURATION = timedelta(days=90)
