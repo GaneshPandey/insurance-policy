3f5f97a05c1598c5e2f498c1eaafaf9d17f1ffa7
GaneshPandey <footballcrazey@gmail.com>
2021-01-31 17:24:03 +0545

    code reformatted by black

--- recommendation/blueprints/user/views.py
+++ recommendation/blueprints/user/views.py
@@ -1,9 +1,13 @@
-from flask import (
-    Blueprint)
+from flask import Blueprint
 
-user = Blueprint('user', __name__, url_prefix="/api/v1/user")
+user = Blueprint("user", __name__, url_prefix="/api/v1/user")
 
 
-@user.route('/', methods=['GET', 'POST'])
+@user.route("/", methods=["GET", "POST"])
 def index():
     return "this is a index route"
+
+
+@user.route("/user", methods=["GET", "POST"])
+def index():
+    return "this is a user route"
