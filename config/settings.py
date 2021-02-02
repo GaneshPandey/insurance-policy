0139eaa52bdd25f91a6502f0c94ff50ee42bd75c
GaneshPandey <footballcrazey@gmail.com>
2021-02-02 19:54:31 +0545

    add new settings

--- config/settings.py
+++ config/settings.py
@@ -1,17 +1,28 @@
 from datetime import timedelta
 
 DEBUG = True
+TESTING = False
 LOG_LEVEL = "DEBUG"  # CRITICAL / ERROR / WARNING / INFO / DEBUG
 
-SERVER_NAME = "www.localhost:8000"
+SERVER_NAME = "localhost:8000"
 SECRET_KEY = "dev-secret-key"
 
 # SQLAlchemy.
-db_uri = "postgresql://devuser:devpassword@postgres:5432/recommendation"
+db_uri = "postgresql://recommendation:recommendationpass123@postgres:5432/recommendation"
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # User.
+SEED_ADMIN_ROLE = "admin"
 SEED_ADMIN_EMAIL = "dev@local.host"
 SEED_ADMIN_PASSWORD = "devpassword"
-REMEMBER_COOKIE_DURATION = timedelta(days=90)
+SEED_ADMIN_FIRST_NAME = "Ganesh"
+SEED_ADMIN_LAST_NAME = "Pandey"
+SEED_ADMIN_USERNAME = "noones"
+
+# bcrypt configuration settings
+# make sure to change accordingly on production settings
+BCRYPT_LOG_ROUNDS = 4
+TOKEN_EXPIRE_HOURS = 0
+TOKEN_EXPIRE_MINUTES = 5
+PRESERVE_CONTEXT_ON_EXCEPTION = False
