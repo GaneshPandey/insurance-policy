393aac70d18e5858691e3e5f935943d31c850bed
GaneshPandey <footballcrazey@gmail.com>
2021-02-02 19:48:30 +0545

    add utils libraries

---
+++ lib/util_sqlalchemy.py
@@ -0,0 +1,23 @@
+import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.types import TypeDecorator
+
+
+class AwareDateTime(TypeDecorator):
+    """
+    A DateTime type which can only store tz-aware DateTimes.
+
+    Source:
+      https://gist.github.com/inklesspen/90b554c864b99340747e
+    """
+
+    impl = DateTime(timezone=True)
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, datetime.datetime) and value.tzinfo is None:
+            raise ValueError("{!r} must be TZ-aware".format(value))
+        return value
+
+    def __repr__(self):
+        return "AwareDateTime()"
